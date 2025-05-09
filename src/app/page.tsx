0de6c3774f2436d886a327f8594940f326369843
Tobias Smith <tobiaswsmith@gmail.com>
2025-05-09 09:25:21 +0000

    Fancy title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -57,7 +57,7 @@ export default function HomePage() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Pointing <span className="text-[hsl(280,100%,70%)]">Poker</span>
+            PZ <span className="text-sm">Pointing</span> <span className="font-bold bg-gradient-to-r from-orange-700 via-blue-500 to-green-400 text-transparent bg-clip-text animate-gradient">Poker</span>
           </h1>
           <div className="grid grid-cols-1 gap-8 md:grid-cols-2 md:gap-12">
             {/* Create Session Form */}
